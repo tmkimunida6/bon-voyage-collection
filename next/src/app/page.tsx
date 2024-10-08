d066749d4ca54b8593d3beda7a1f399a0b5064f0
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-08 09:06:32 +0900

    test: test auto deploy again

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>test deploy!</li>
+          <li>test deploy again!</li>
         </ol>
 
         <div className={styles.ctas}>
