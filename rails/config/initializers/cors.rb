8418552d33e72c32ba5120781416d49d15cba43d
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-08 23:02:33 +0900

    feat: create cors setting

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "0.0.0.0:3001", "localhost:3001", "https://souvenir-99j28dnek-tmk-imunida6s-projects.vercel.app/"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
