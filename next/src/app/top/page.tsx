b82b14d2702f960da925c2880c89467b713aabba
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-10 00:36:55 +0900

    chore: install chakra ui

--- next/src/app/top/page.tsx
+++ next/src/app/top/page.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import { Button } from '@chakra-ui/react'
 
 const Top: NextPage = () => {
   return (
     <>
       <h1>Top</h1>
+      <Button colorScheme='blue'>Button</Button>
     </>
   )
 }
