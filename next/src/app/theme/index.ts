ff69efc87dc56b651f5deb4a80b0049dd855997b
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-13 00:30:29 +0900

    style: arrange base style

---
+++ next/src/app/theme/index.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from '@chakra-ui/react'
+import { Button } from './components/button'
+import { Link } from './components/link'
+
+const customTheme = extendTheme({
+  colors: {
+    brand: {
+      primary: '#E699AA',
+      secondary: '#F2DEE8',
+      gray: '#C8D0DB',
+      brown: '#C69B92',
+      link: '#88BFB8',
+      star: '#E2DB87'
+    },
+  },
+  components: {
+    Button,
+    Link,
+  },
+})
+
+export default customTheme
