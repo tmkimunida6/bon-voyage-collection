51c7437fd653064dd9e40da308a8501d218b978c
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-13 16:14:56 +0900

    test: test display components

--- next/src/app/top/page.tsx
+++ next/src/app/top/page.tsx
@@ -1,25 +1,77 @@
-import { Button } from '@chakra-ui/react'
+'use client'
+
+import {
+  Button,
+  Input,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Accordion,
+  Text,
+} from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import CustomAccordionItem from '../components/molecules/CustomAccordionItem'
 import TextIconLink from '../components/molecules/TextIconLink'
-import SouvenirCardList from '../components/organisms/souvenir/SouvenirCardList'
+import TriggerWithModal from '../components/organisms/modal/TriggerWithModal'
 import SouvenirCard from '../components/organisms/souvenir/SouvenirCard'
+import SouvenirCardList from '../components/organisms/souvenir/SouvenirCardList'
 
 const Top: NextPage = () => {
   return (
     <>
       <h1>Top</h1>
       <Button variant="primary">おすすめのお土産をみる</Button>
-      <TextIconLink iconName="FaPenSquare" iconPosition='left' href="#">編集する</TextIconLink>
-      <SouvenirCardList 
-        size='lg'
+      <TextIconLink iconName="FaPenSquare" iconPosition="left" href="#">
+        編集する
+      </TextIconLink>
+      <SouvenirCardList
+        size="lg"
         renderItem={(size) => (
           <>
             <SouvenirCard size={size} isFavoritable={true} />
-            <SouvenirCard size={size} isFavoritable={true}/>
+            <SouvenirCard size={size} isFavoritable={true} />
             <SouvenirCard size={size} isFavoritable={true} />
           </>
         )}
       />
+      <TriggerWithModal
+        renderTrigger={({ onClick }) => (
+          <Input placeholder="Click to open modal" onClick={onClick} />
+        )}
+        modalTitle="詳細条件"
+        buttonText="確定"
+      >
+        <Tabs>
+          <TabList>
+            <Tab>One</Tab>
+            <Tab>Two</Tab>
+            <Tab>Three</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </TriggerWithModal>
+      <Accordion allowToggle>
+        <CustomAccordionItem
+          title="アコーディオンを開く"
+          triggerBgColor="brand.gray"
+          hasBorder={true}
+        >
+          <Text>あああああああああああ</Text>
+        </CustomAccordionItem>
+      </Accordion>
     </>
   )
 }
