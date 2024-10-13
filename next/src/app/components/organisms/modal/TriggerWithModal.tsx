8327c2a969af08f45536156f045a3739f096228f
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-13 15:21:31 +0900

    feat: create modal component

---
+++ next/src/app/components/organisms/modal/TriggerWithModal.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { ReactElement, ReactNode } from 'react'
+import { useDisclosure } from '@chakra-ui/react'
+import CustomModal from './CustomModal'
+
+type TriggerWithModalProps = {
+  renderTrigger: (props: { onClick: () => void }) => ReactElement,
+  modalTitle: string,
+  buttonText: string,
+  children: ReactNode
+}
+
+const TriggerWithModal = ({ renderTrigger, modalTitle, buttonText, children }: TriggerWithModalProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      {renderTrigger({onClick: onOpen})}
+      <CustomModal isOpen={isOpen} onClose={onClose} modalTitle={modalTitle} buttonText={buttonText} >
+        {children}
+      </CustomModal>
+    </>
+  )
+}
+
+export default TriggerWithModal;
