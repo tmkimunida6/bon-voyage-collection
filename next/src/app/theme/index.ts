acb2a8d9d68376ea64a6854520566b26c8644f3f
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-13 16:00:07 +0900

    feat: create template and arrange layout

--- next/src/app/theme/index.ts
+++ next/src/app/theme/index.ts
@@ -1,8 +1,21 @@
 import { extendTheme } from '@chakra-ui/react'
+import { Noto_Sans_JP } from 'next/font/google'
 import { Button } from './components/button'
 import { Link } from './components/link'
 
+const notoSansJp = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  preload: false,
+  display: 'swap',
+  fallback: ['Hiragino Sans', 'Hiragino Kaku Gothic ProN', 'sans-serif'],
+})
+
 const customTheme = extendTheme({
+  fonts: {
+    heading: notoSansJp.style.fontFamily,
+    body: notoSansJp.style.fontFamily,
+  },
   colors: {
     brand: {
       primary: '#E699AA',
