ebd32e11ac25be4daa8fca915a5ee44856d0d5f5
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-14 14:02:29 +0900

    chore: set api base url in env files

--- next/src/app/test/page.tsx
+++ next/src/app/test/page.tsx
@@ -2,10 +2,11 @@
 
 import type { NextPage } from 'next'
 import useSWR from 'swr'
+import { apiBaseUrl } from '@/constants/apiBaseUrl'
 import { fetcher } from '@/utils/fetcher'
 
 const Test: NextPage = () => {
-  const url = 'http://localhost:3001/api/v1/health_check'
+  const url = `${apiBaseUrl}/api/v1/health_check`
   const { data, error } = useSWR(url, fetcher)
 
   if (error) return <div>An error has occurred.</div>
