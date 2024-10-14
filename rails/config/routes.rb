55b7dc9bae001583af5b0362348f54eb3ec0af48
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-14 20:05:23 +0900

    feat: install letter_opner_web and email auth test

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "up", to: "rails/health#show", as: :rails_health_check
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
