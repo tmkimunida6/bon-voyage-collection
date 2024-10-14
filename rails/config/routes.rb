5ece599bdcee28c531500ef2a88e129fbaf81271
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-15 00:37:12 +0900

    feat: create current user API

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
+  # renderヘルスチェック
   get "up", to: "rails/health#show", as: :rails_health_check
+
+  # メールテスト用
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  # API
   namespace :api do
     namespace :v1 do
+      # ヘルスチェック
       get "health_check", to: "health_check#index"
+
+      # devise認証
       mount_devise_token_auth_for "User", at: "auth"
+
+      # ログイン中のユーザー
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
 end
