bd154f4c9f9a01d6033388dfe6b9084942060a5a
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-10-15 10:46:20 +0900

    test: create user and healt_check spec

---
+++ rails/spec/requests/api/v1/current/users_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Current::Users", type: :request do
+  describe "GET /api/v1/current/user" do
+    subject(:get_request) { get(api_v1_current_user_path, headers:) }
+    let(:current_user) { create(:user) }
+    let(:headers) { current_user.create_new_auth_token }
+
+    context "ヘッダー情報が正常に送られた場合" do
+      it "正常にレコードを取得できる" do
+        get_request
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq ["id", "name", "email"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "ヘッダー情報がからの場合" do
+      let(:headers) { nil }
+
+      it "権限エラーが返却される" do
+        get_request
+        res = JSON.parse(response.body)
+        expect(res["errors"]).to eq ["ログインもしくはアカウント登録してください。"]
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+end
--- rails/spec/requests/api/v1/health_check_spec.rb
+++ rails/spec/requests/api/v1/health_check_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::HealthChecks", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/v1/health_check" do
+    it "正常なレスポンスが返却される" do
+      get(api_v1_health_check_path)
+      res = JSON.parse(response.body)
+      expect(res["message"]).to eq("Success Health Check")
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
