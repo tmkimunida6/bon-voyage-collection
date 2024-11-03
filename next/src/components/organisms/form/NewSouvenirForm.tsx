566bcc97d967d339a51fc38bd4f1d28409518b91
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-11-03 15:41:40 +0900

    fix: fix lint error

--- next/src/components/organisms/form/NewSouvenirForm.tsx
+++ next/src/components/organisms/form/NewSouvenirForm.tsx
@@ -6,7 +6,7 @@ import {
   AlertIcon,
   FormControl,
   FormLabel,
-  Stack
+  Stack,
 } from '@chakra-ui/react'
 import { useForm } from '@conform-to/react'
 import { parseWithZod } from '@conform-to/zod'
