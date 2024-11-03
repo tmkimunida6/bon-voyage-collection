04e4b0ef97ff83db4b59745cffdf58ea7cf13c7b
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-11-03 14:00:59 +0900

    feat: add uniqueness validation to souvenir

--- rails/app/models/souvenir.rb
+++ rails/app/models/souvenir.rb
@@ -2,5 +2,5 @@ class Souvenir < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_29_131914) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_03_001649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_29_131914) do
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
     t.index ["category_id"], name: "index_souvenirs_on_category_id"
+    t.index ["name"], name: "index_souvenirs_on_name", unique: true
     t.index ["user_id"], name: "index_souvenirs_on_user_id"
   end
 
