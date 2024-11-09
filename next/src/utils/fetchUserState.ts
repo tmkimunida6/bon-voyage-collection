d715f6be5ba790209555cee1c32ba62df6079bc4
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-11-09 15:39:48 +0900

    fix: fix build error

--- next/src/utils/fetchUserState.ts
+++ next/src/utils/fetchUserState.ts
@@ -6,7 +6,7 @@ import { UserType } from '@/types/types'
 
 export async function fetchUserState() {
   let user: UserType = {
-    id: 0,
+    alias_id: 0,
     name: '',
     email: '',
     isSignedIn: false,
