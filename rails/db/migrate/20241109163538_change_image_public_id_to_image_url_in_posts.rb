e9332f392effcf716a592ed0b970cd53ec7c753b
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-11-10 15:15:59 +0900

    fix: fix migration error

--- rails/db/migrate/20241109163538_change_image_public_id_to_image_url_in_posts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeImagePublicIdToImageUrlInPosts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :posts, :image_url, :string
-    add_index :posts, :image_url, unique: true
-  end
-end
--- rails/db/migrate/20241110031457_add_not_null_constraint_to_image_url_in_souvenirs.rb
+++
@@ -1,5 +0,0 @@
-class AddNotNullConstraintToImageUrlInSouvenirs < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :souvenirs, :image_url, false
-  end
-end
