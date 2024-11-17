4de1288c19c555fdd029881711828f97176b72a7
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2024-11-17 11:28:17 +0900

    feat: set favicon

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -18,6 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Bon Voyage" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </head>
       <body>
         <Providers>
           <Suspense
