fbee6da20c8634a215e5c14d25d9a190fcc40cc2
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2025-01-12 15:29:22 +0900

    feat: override devise method

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -17,6 +17,23 @@ class User < ActiveRecord::Base
 
   validates :image, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "は有効なURLを入力してください。" }, allow_blank: true
 
+
+  # メールアドレス変更時は別のテンプレートを使用するように上書き
+  def send_confirmation_instructions(opts = {})
+    generate_confirmation_token! unless @raw_confirmation_token
+
+    # fall back to "default" config name
+    opts[:client_config] ||= 'default'
+    opts[:to] = unconfirmed_email if pending_reconfirmation?
+    opts[:redirect_url] ||= DeviseTokenAuth.default_confirm_success_url
+
+    if pending_reconfirmation?
+      send_devise_notification(:email_change_confirmation_instructions, @raw_confirmation_token, opts)
+    else
+      send_devise_notification(:confirmation_instructions, @raw_confirmation_token, opts)
+    end
+  end
+
   protected
 
   # パスワードの変更以外はパスワード入力なしで変更可能にする
