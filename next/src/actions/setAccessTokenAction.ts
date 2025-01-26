72bf0bd838d875a7c6af40d73a62f3413f29be79
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2025-01-27 01:03:03 +0900

    fix: fix cookie setting

--- next/src/actions/setAccessTokenAction.ts
+++ next/src/actions/setAccessTokenAction.ts
@@ -10,8 +10,8 @@ export async function setAccessTokenAction(
   const cookieStore = await cookies()
   cookieStore.set('access-token', accessToken, {
     httpOnly: true,
-    secure: false,
+    secure: true,
   })
-  cookieStore.set('client', client, { httpOnly: true, secure: false })
-  cookieStore.set('uid', uid, { httpOnly: true, secure: false })
+  cookieStore.set('client', client, { httpOnly: true, secure: true })
+  cookieStore.set('uid', uid, { httpOnly: true, secure: true })
 }
