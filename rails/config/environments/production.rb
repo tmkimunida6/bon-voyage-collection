3b3e7d566a556cf46b955c86f018358d68a61093
tmkimunida6 <yuguchi.tomoki.p0@gmail.com>
2025-02-08 17:44:35 +0900

    fix: change domain setting

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
+    "souvenir-cszk.onrender.com",
     "bon-voyage-collection.com",     # Allow requests from example.com
     # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   ]
